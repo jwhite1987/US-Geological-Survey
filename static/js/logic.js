3ac1f9622b68611de49a1ca0258470a42dc5c883
Jimmy <>
2021-04-26 23:50:03 -0400

    final

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,44 +1,95 @@
-function createMap(earthquakes) {
 
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY
-  });
-  var baseMap = {
-    "Light Map": lightmap
-  };
-  var overlayMap = {
-    "Earthquakes": earthquakes
-  };
 
-  var map = L.map("mapid", {
-    center: [50, 29],
-    zoom: 12,
-    layers: [lightmap, earthquakes]
-  });
+var map = L.map("map", {
+  center: [50.67, 29.79],
+  zoom: 3,
+  attributionControl: false
+});
 
-  L.control.layers(baseMap, overlayMap, {
-    collapsed: false
-  }).addTo(map);
-}
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  // attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  maxZoom: 18,
+  id: "streets-v11",
+  accessToken: API_KEY
+}).addTo(map);
 
-function createMarkers(response) {
+// L.control.layers(baseMaps, overlayMaps, {
+//   collapsed: false
+// }).addTo(map);
+
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function(response) {
   var earthquakes = response.features;
   console.log(earthquakes)
-  var earthquakeMarkers = [];
-
+  // var markers = L.markerClusterGroup();
+  L.geoJson(response).addTo(map);
   for (var index = 0; index < earthquakes.length; index++) {
     var earthquake = earthquakes[index].geometry.coordinates;
-    console.log(earthquake)
-    var earthquakeMarker = L.marker([earthquake[0], earthquake[1]])
-    .bindPopup("EARTHQUAKEEEE");
+    console.log(earthquake[0], earthquake[1]);
+    L.circleMarker([earthquake[0], earthquake[1]]).addTo(map)
+    .bindPopup("EARTHQUAKEEEE"); }
 
-    earthquakeMarkers.push(earthquakeMarker);
-  }
-  createMap(L.layerGroup(earthquakeMarkers));
-  console.log(earthquakeMarkers)
-}
+    geojson = L.choropleth(data, {
 
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson").then(createMarkers);
+      // Define what  property in the features to use
+      valueProperty: "MHI2016",
+
+      // Set color scale
+      scale: ["#ffffb2", "#b10026"],
+
+      // Number of breaks in step range
+      steps: 10,
+
+      // q for quartile, e for equidistant, k for k-means
+      mode: "q",
+      style: {
+        // Border color
+        color: "#fff",
+        weight: 1,
+        fillOpacity: 0.8
+      },
+
+      // Binding a pop-up to each layer
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
+          "$" + feature.properties.MHI2016);
+      }
+    }).addTo(map);
+
+
+
+    // Set up the legend
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+      var limits = geojson.options.limits;
+      var colors = geojson.options.colors;
+      var labels = [];
+
+      // Add min & max
+      var legendInfo = "<h1>Median Income</h1>" +
+        "<div class=\"labels\">" +
+          "<div class=\"min\">" + limits[0] + "</div>" +
+          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
+
+      div.innerHTML = legendInfo;
+
+      limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+      });
+
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+      return div;
+    };
+
+    // Adding legend to the map
+    legend.addTo(map);
+
+
+
+
+
+
+});
